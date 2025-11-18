d3b6465728eba0be0fb81470dbe453a909586c98
Dark Engineer <maniodark@gmail.com>
2025-11-18 01:07:48 +0100

    zoneless to service tests

--- projects/dark-text-editor/src/lib/parser/parser-service.spec.ts
+++ projects/dark-text-editor/src/lib/parser/parser-service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ParserService } from './parser-service';
+import { provideZonelessChangeDetection } from '@angular/core';
 
 describe('ParserService', () => {
   let service: ParserService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [provideZonelessChangeDetection()]
+    });
     service = TestBed.inject(ParserService);
   });
 
--- projects/dark-text-editor/src/lib/utils/utils-service.spec.ts
+++ projects/dark-text-editor/src/lib/utils/utils-service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
 import { UtilsService } from './utils-service';
+import { provideZonelessChangeDetection } from '@angular/core';
 
 describe('UtilsService', () => {
   let service: UtilsService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [provideZonelessChangeDetection()]
+    });
     service = TestBed.inject(UtilsService);
   });
 
